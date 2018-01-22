29753e4cd7c2740183030f24b1b754e7922ce343
jnasciment0 <jasson.segundo@gmail.com>
2018-01-22 16:58:06 -0300

    Exemplo da impressora e exercicios

---
+++ capitulo_8/exercicio_8_11.py
@@ -0,0 +1,22 @@
+magicos_nomes = ['Jasson','Huding', 'Mrs. M']
+nomes_novos = []
+
+def show_magiccians(magicos_nomes):
+	for magicos_nome in magicos_nomes:
+		print("Apresento a vocês os magicos: \n" + magicos_nome.title())
+
+def make_great(magicos_nomes,nomes_novos):
+	
+	while magicos_nomes:
+		recebe_nome = magicos_nomes.pop()
+		##print("magic: " +recebe_nome )
+		nomes_novos.append('O grande ' + recebe_nome)
+	
+	for nomes_novo in nomes_novos:
+		print("Lista alterada : "+ nomes_novo)
+
+	for magicos_nome in magicos_nomes:
+		print("Lista original \n" + magicos_nomes)
+
+show_magiccians(magicos_nomes)
+make_great(magicos_nomes[:],nomes_novos)
---
+++ capitulo_8/exercicio_8_9.py
@@ -0,0 +1,7 @@
+magicos_nomes = ['Jasson','Huding', 'Mrs. M']
+
+def show_magiccians(magicos_nome):
+	for magicos_nome in magicos_nomes:
+		print("Apresento a vocês o grande magico: \n" + magicos_nome.title())
+
+show_magiccians(magicos_nomes)
