eafb7841d79a5002e4fa0d4e8db238024bba11f1
jnasciment0 <jasson.segundo@gmail.com>
2018-06-18 11:07:20 -0300

    capitulo 9

--- capitulo_9/car.py
+++ capitulo_9/car.py
@@ -5,7 +5,8 @@ class Car():
     self.make = make
     self.model = model
     self.year = year
-    self.odometer_reading = 5
+    self.odometer_reading = 0
+    
 
   def get_descriptive_name(self):
     """Devolver um nome descritivo, formatado de modeo elegante"""
@@ -29,16 +30,34 @@ class Car():
     """Soma a quantidade especifica ao valor de leitura do hodômetro"""
     self.odometer_reading += miles
 
-my_used_car = Car('subaru', 'outbak', 2013)
-print(my_used_car.get_descriptive_name())
+class Battery():
+  """uma tentativa simples de modela uma bateria para um carro eletrico"""
+  def __init__(self, battery_size = 60):
+    """Inicialza os atributos da baterias"""
+    self.battery_size = battery_size
 
-my_used_car.update_odometer(23500)
-my_used_car.read_odometer()
+  def describe_battery(self):
+    """Exebi uma frase que descreve a capacidade da bateria"""
+    print("This car has a " + str(self.battery_size) + "-kwh battery.")
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
-# my_new_car = Car('audi','a4',2016);
-# print(my_new_car.get_descriptive_name( ));
-# my_new_car.odometer_reading = 23;
-# my_new_car.update_odometer(1);
-# my_new_car.read_odometer();
+  def get_range(self):
+    """Exebi uma frase sobre a distancia que o carro é capaz de percorre com essa bateria"""
+    range = 0
+    if self.battery_size == 70:
+      range = 240
+    elif self.battery_size == 85:
+      range = 270
+  
+class ElectricCar(Car):
+  """Representa aspectos de um carro especificos de veiculos eletricos"""
+  def __init__(self, make, model,year):
+    """
+    Iniciando atributos do pai.
+    Em seguida, inicializa os atributos especificos de um carro eletrico
+    """
+    super().__init__(make, model, year)
+    self.battery = Battery()
+    
+  def fill_gas_tank():
+    """Carros eletricos não tem tanques de gasolina"""
+    print("This car doesn't nees a gas tank!")
